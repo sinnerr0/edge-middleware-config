7e54adf534dafb60c1368f0cccc82b32898b04e4
kyongsikC <kyeongsik.choi@greenlabs.co.kr>
2023-01-12 02:22:02 +0900

    set cookies

--- middleware.ts
+++ middleware.ts
@@ -5,8 +5,6 @@ import type { NextRequest } from "next/server";
 export async function middleware(req: NextRequest) {
   const featureFlags = await get("feature-flags");
   console.log(`feature-flags=${featureFlags}`);
-
-  return next({
-    headers: { "x-feature-flags": featureFlags },
-  });
+  req.cookies.set("feature-flags", `${featureFlags}`);
+  return next();
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const cookie = document.cookie;
   return (
     <>
       <Head>
@@ -9,7 +10,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <div>{cookie}</div>
+      </main>
     </>
   );
 }
