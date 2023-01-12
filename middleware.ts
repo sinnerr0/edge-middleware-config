3bd810212764b8b5428e0136b9372959645e2e55
kyongsikC <kyeongsik.choi@greenlabs.co.kr>
2023-01-12 09:24:56 +0900

    remove api

--- middleware.ts
+++ middleware.ts
@@ -5,6 +5,6 @@ import type { NextRequest } from "next/server";
 export async function middleware(req: NextRequest) {
   const featureFlags = await get("feature-flags");
   console.log(`feature-flags=${featureFlags}`);
-  req.cookies.set("feature-flags", `${featureFlags}`);
+  req.cookies.set("feature-flags", `${JSON.stringify(featureFlags)}`);
   return next();
 }
