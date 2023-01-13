1cd967d8ddbc36e2c651816cff75c5f3d5f076f0
kyongsikC <kyeongsik.choi@greenlabs.co.kr>
2023-01-13 18:31:18 +0900

    chore: add statsig

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,36 @@
 import Head from "next/head";
 import Cookies from "js-cookie";
 import { useEffect, useState } from "react";
-import styles from "../styles/Home.module.css";
+import Statsig, { DynamicConfig } from "statsig-js";
+
+async function StatsigInit() {
+  await Statsig.initialize(
+    process.env.NEXT_PUBLIC_STATSIG_CLIENT_KEY as string
+  );
+  const config: DynamicConfig = Statsig.getConfig("jaje-common");
+  return config;
+}
 
 export default function Home() {
   const [cookie, setCookie] = useState("");
+  const [config, setConfig] = useState("");
   useEffect(() => {
     const features = Cookies.get("feature-flags");
-    features && setCookie(features);
+    features && setCookie(JSON.stringify(JSON.parse(features), null, 2));
+    (async () => {
+      const config = await StatsigInit();
+      setConfig(
+        JSON.stringify(
+          {
+            frontend: config.getValue("frontend"),
+            backend: config.getValue("backend"),
+            common: config.getValue("common"),
+          },
+          null,
+          2
+        )
+      );
+    })();
   }, []);
   return (
     <>
@@ -16,8 +39,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div>{cookie}</div>
+      <main>
+        <h1>Edge Config</h1>
+        <pre>{cookie}</pre>
+        <h1>Statsig Config</h1>
+        <pre>{config}</pre>
       </main>
     </>
   );
